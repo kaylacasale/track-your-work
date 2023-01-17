6506ea99a86170c07605ba6ab942b48dfa85af06
Kayla Casale <kayla.casale@gmail.com>
2023-01-16 19:14:49 -0700

    finally debugged and figured out how to console.table all departments

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //* npm install --save mysql2
 const express = require('express');
+const inquirer = require('inquirer')
+const cTable = require('console.table');
 
 //* import and require mysql2
 const mysql = require('mysql2');
@@ -9,6 +11,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 
 const db = mysql.createConnection(
     {
@@ -21,6 +24,49 @@ const db = mysql.createConnection(
     console.log(`Connected to the employee_tracker_db database.`)
 );
 
+function startPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'menu',
+                message: 'Please select one.',
+                choices: ['View all departments', 'View all Roles', 'View all employees', 'Add a department.', 'Add a role.', 'Add an employee.', 'Update an employee role.']
+            }
+        ])
+        .then((answer) => {
+            switch (answer.menu) {
+                case 'View all departments':
+                    viewAllDepartments();
+                    break;
+                case 'View all roles':
+                    viewAllRoles();
+                    break;
+                case 'View all employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add a department':
+                    addADepartment();
+                    break
+                case 'Add a role':
+                    addARole();
+                    break
+                case 'Add an empoloyee':
+                    addAnEmployee();
+                    break
+                case 'Update en employee role':
+                    updateAnEmployeeRole()
+                    break
+            }
+        })
+};
+
+function viewAllDepartments() {
+    db.query('SELECT * FROM department', function (err, results) {
+        console.table(results);
+    });
+}
+
 
 app.use((req, res) => {
     res.status(404).end();
@@ -29,3 +75,5 @@ app.use((req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+startPrompt()
